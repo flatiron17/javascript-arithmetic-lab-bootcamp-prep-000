c6abe780e1de514a1f97bcd18272ad5de933c149
oscar Diaz <oscarlidiaz@gmail.com>
2018-04-26 22:44:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function add(a,b){
+  return a+b
+}
+function subtract(a,b) {
+  return a+b
+}
+function multiply(a,b) {
+  return a+b
+}
+function divide(a,b){
+  return a+b
+}
+function increment(1) {
+  return 1
+}
+function decrement(1) {
+  return 1
+}
